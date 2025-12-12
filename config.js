a2fa4039c4b358d1638de5492f125d5c09c49ae8
Sergey <82959536+loroulora@users.noreply.github.com>
2025-12-12 14:12:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,5 @@
-// config.js - КОНФИГУРАЦИЯ ПРИЛОЖЕНИЯ
-// ЭТОТ ФАЙЛ НЕЛЬЗЯ ДОБАВЛЯТЬ В GIT!
-// config.js - КОНФИГУРАЦИЯ ПРИЛОЖЕНИЯ (ФРОНТЕНД)
-// ЭТОТ ФАЙЛ НЕЛЬЗЯ ДОБАВЛЯТЬ В GIT!
-
 window.APP_CONFIG = {
-    // URL твоего Google Apps Script
     API_URL: 'https://script.google.com/macros/s/AKfycbwLGtk5t5mK-CwR7hEKcYlIDM6R6JqIv1NeKsAztDBTK7zewiLQjbLyxk2CHU3DGqZx/exec',
-    
-    // Настройки приложения
     APP_NAME: 'Система судейства',
     VERSION: '1.0.0'
 };
