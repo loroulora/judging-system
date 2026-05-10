eb8a3b403dcf10b45ef31d999a6acf3846b88362
Sergey <82959536+loroulora@users.noreply.github.com>
2026-05-10 17:18:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
     API_URL: 'https://script.google.com/macros/s/AKfycbyO4MXymmhXZoNnDa1Sxss2sVsi4gQLhPLWT8MJfUZuAHb8k2t5B24MomehklkLKBsU/exec',
     APP_NAME: 'Система судейства',
-    VERSION: '1.0.0'
+    VERSION: '1.1.0' 
 };
